319afd853d60b38050a7beea87785bf184b9fa4f
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-12-17 18:37:23 +0900

    記号をcodeに統一する。

--- miu.thy
+++ miu.thy
@@ -1,47 +1,50 @@
+(* GEB MIU Puzzle *)
+
 theory "miu"
   imports Main
+
 begin
 
 datatype miu = M | I | U
 
-inductive_set S :: "miu list set" where
-  "[M, I] : S" |
-  "x @ [I] : S ==> x @ [I, U] : S" |
-  "[M] @ x : S ==> [M] @ x @ x : S" |
-  "x @ [I, I, I] @ y : S ==> x @ [U] @ y : S" |
-  "x @ [U, U] @ y : S ==> x @ y : S"
+inductive_set MIU :: "miu list set" where
+  "[M, I] : MIU" |
+  "x @ [I] : MIU \<Longrightarrow> x @ [I, U] : MIU" |  (* \<Longrightarrow> abbreviation is "= = >" *)
+  "[M] @ x : MIU \<Longrightarrow> [M] @ x @ x : MIU" |
+  "x @ [I, I, I] @ y : MIU \<Longrightarrow> x @ [U] @ y : MIU" |
+  "x @ [U, U] @ y : MIU \<Longrightarrow> x @ y : MIU"
 
-fun ci :: "miu list => nat" where
+fun ci :: "miu list \<Rightarrow> nat" where        (* \<Rightarrow> abbreviation is "= >" *)
   "ci [] = 0" |
-  "ci (x#xs) = (if x = I then 1 + (ci xs) else ci xs)"
+  "ci (x # xs) = (if x = I then ci xs + 1 else ci xs)"
 
-lemma [rule_format,simp]: "ALL y. ci (x @ y) = ci x + ci y"
-  apply(induct_tac x)
-   apply(auto)
+lemma [rule_format, simp] : "\<forall> y. ci (x @ y) = ci x + ci y"
+  apply (induct_tac x)
+   apply (auto)
+  done
+                                              (* \<noteq> abbs "~ =" *)
+lemma miu_inv : "x \<in> MIU \<Longrightarrow> ci x mod 3 \<noteq> 0"  (* \<in> abbs "i n" *)
+  apply (erule MIU.induct)
+      apply (auto)
+  apply (arith)
   done
 
-lemma miu_inv: "x : S ==> (ci x) mod 3 ~= 0"
-  apply(erule S.induct)
-      apply(auto)
-  apply(arith)
+theorem th_miu' : "[M, U] \<notin> MIU"    (* \<notin> abbs "~ :" *)
+  apply (rule classical)
+  apply (simp)
+  apply (drule miu_inv)
+  apply (simp)
   done
 
-theorem th_miu : "[M, U] ~: S"
-  apply(rule classical)
-  apply(simp)
-  apply(drule miu_inv)
-  apply(simp)
-  done
-
-theorem th_miu2 :
-  shows "[M, U] ~: S"
-proof (rule classical)  (* 1. ¬ [M, U] ∉ S ⟹ [M, U] ∉ S *)
-  assume "~ [M, U] ~: S"
-  from this have 1 : "[M, U] : S" by simp
-  from this have "ci [M, U] mod 3  ~= 0" by (rule miu_inv)
-  from this have  2: "~ [M, U] : S" by simp
+theorem th_miu1 : "[M, U] \<notin> MIU"
+  proof (rule classical)  (*  \<not> [M, U] \<notin> S \<Longrightarrow> [M, U] \<notin> S *)
+    assume "\<not> [M, U] \<notin> MIU"
+  from this have 1 : "[M, U] : MIU" by simp
+  from this have "ci [M, U] mod 3  \<noteq> 0" by (rule miu_inv)
+  from this have  2: "\<not> [M, U] : MIU" by simp
   from 1 and 2 have  "False" by simp
-  from this show "[M, U] ∉ S" by simp
+  from this show "[M, U] \<notin> MIU" by simp
 qed
 
-end
+(* END *)
+
