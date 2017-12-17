654adbdb8f0467d8580584375e40d082278fdbb6
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-12-17 18:46:19 +0900

    hence と thus を使う。

--- miu.thy
+++ miu.thy
@@ -29,22 +29,32 @@ lemma miu_inv : "x \<in> MIU \<Longrightarrow> ci x mod 3 \<noteq> 0"  (* \<in> 
   apply (arith)
   done
 
-theorem th_miu' : "[M, U] \<notin> MIU"    (* \<notin> abbs "~ :" *)
+theorem th_miu'' : "[M, U] \<notin> MIU"    (* \<notin> abbs "~ :" *)
+  (* \<not> [M, U] \<notin> S \<Longrightarrow> [M, U] \<notin> S *)
   apply (rule classical)
   apply (simp)
   apply (drule miu_inv)
   apply (simp)
   done
 
-theorem th_miu1 : "[M, U] \<notin> MIU"
-  proof (rule classical)  (*  \<not> [M, U] \<notin> S \<Longrightarrow> [M, U] \<notin> S *)
-    assume "\<not> [M, U] \<notin> MIU"
+theorem th_miu' : "[M, U] \<notin> MIU"
+proof (rule classical)  (* \<not> [M, U] \<notin> S \<Longrightarrow> [M, U] \<notin> S *)
+  assume "\<not> [M, U] \<notin> MIU"
   from this have 1 : "[M, U] : MIU" by simp
-  from this have "ci [M, U] mod 3  \<noteq> 0" by (rule miu_inv)
+  from this have "ci [M, U] mod 3 \<noteq> 0" by (rule miu_inv)
   from this have  2: "\<not> [M, U] : MIU" by simp
-  from 1 and 2 have  "False" by simp
+  from 1 and 2 have "False" by simp
   from this show "[M, U] \<notin> MIU" by simp
 qed
 
-(* END *)
+theorem th_miu : "[M, U] \<notin> MIU"
+proof (rule classical)  (* \<not> [M, U] \<notin> S \<Longrightarrow> [M, U] \<notin> S *)
+  assume "\<not> [M, U] \<notin> MIU"
+  hence 1 : "[M, U] : MIU" by simp
+  hence "ci [M, U] mod 3 \<noteq> 0" by (rule miu_inv)
+  hence 2: "\<not> [M, U] : MIU" by simp
+  from 1 and 2 have "False" by simp
+  thus "[M, U] \<notin> MIU" by simp
+qed
 
+(* END *)
