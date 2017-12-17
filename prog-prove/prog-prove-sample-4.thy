52a8bcc6f45cf6cdd4f0bce9fa718a619a4eb8bd
SUHARA Hiromichi <suharahiromichi@gmail.com>
2017-12-17 23:05:41 +0900

    他のサンプルを追加する。

--- prog-prove/prog-prove-sample-4.thy
+++ prog-prove/prog-prove-sample-4.thy
@@ -8,14 +8,52 @@ theory "prog-prove-sample-4"  (* example codes  *)
 
 begin
 
-lemma test : "\<not> surj (f :: 'a  \<Rightarrow>  'a set)"
+lemma sample1 : "\<not> surj (f :: 'a \<Rightarrow> 'a set)"
 proof
-assume 0: "surj f"
+  assume 0: "surj f"
   from 0 have 1: " \<forall> A. \<exists> a. A = f a" by (simp add: surj_def)
   from 1 have 2: "EX a. {x . x \<notin> f x} = f a" by blast
   from 2 show "False" by blast
 qed
 
-(* a sample of the obtain *)
+lemma sample2 : "\<not> surj (f :: 'a \<Rightarrow> 'a set)"
+proof
+  assume 0: "surj f"
+  have 1: " \<forall> A. \<exists> a. A = f a" using 0 by (simp add: surj_def)
+  have 2: "EX a. {x . x \<notin> f x} = f a" using 1 by blast
+  show "False" using 2 by blast
+qed
+
+lemma sample3 : "\<not> surj (f :: 'a \<Rightarrow> 'a set)"
+proof
+  assume "surj f"
+  from this have "\<exists> a. {x . x \<notin> f x} = f a" by(auto simp: surj_def)
+  from this show "False" by blast
+qed
+
+lemma sample4 : "\<not> surj (f :: 'a \<Rightarrow> 'a set)"
+proof
+  assume "surj f"
+  hence "\<exists> a. {x . x \<notin> f x} = f a" by (auto simp: surj_def)
+  thus "False" by blast
+qed
+
+lemma sample5 :
+  fixes f :: " 'a \<Rightarrow> 'a set"
+  assumes s: "surj f"
+  shows "False"
+proof -
+  have "\<exists> a. {x . x \<notin> f x} = f a" using s by (auto simp: surj_def)
+  thus "False" by blast
+qed
+
+lemma sample6 : "\<not> surj (f :: 'a \<Rightarrow> 'a set)"
+proof
+  assume "surj f"
+  hence "\<exists> a. {x . x \<notin> f x} = f a" by (auto simp: surj_def)
+  then obtain a where "{x . x \<notin> f x} = f a" by blast
+  hence "a \<notin> f a \<longleftrightarrow> a \<in> f a" by blast
+  thus "False" by blast
+qed
 
 end
